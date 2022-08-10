e0a30f5cbe31e7ea7c1f18f9f614f1ca769c8a26
reddyvarisrinu95 <ec2-user@ip-172-31-8-138.ap-southeast-1.compute.internal>
2022-08-10 05:51:04 +0000

    updated java file

--- user.java
+++ user.java
@@ -1,3 +1,6 @@
-hi this is srinu 
-iam using user.java extension file
-
+ 
+  Iam using user.java extension file
+  Having around 4+ of experience in as DevOps Engineer and also Application support.
+  Experience in SCM tools GIT, Git hub
+  Experience in DevOps Engineer to understand the melding of operations and development to ensure quick delivery of projects to the clients.
+  knowledge on development methodologies especially Agile
